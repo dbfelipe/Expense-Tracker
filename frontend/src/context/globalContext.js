e3e66993b13e32092738f2a7bb1880dc45b89209
Daniel B Felipe <danielbfelipe@Daniels-MacBook-Pro-9.local>
2024-11-06 09:46:26 -0600

    Updated BASE_URL to include /api/v1 for backend routes

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5002/api/v1/";
+//const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5002/api/v1/";
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}/api/v1/`;
+
 
 const GlobalContext = React.createContext();
 
